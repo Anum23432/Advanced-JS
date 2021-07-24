3e4c5371c265b2c90231002809faf056d4a6943e
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-07-24 19:45:18 +0500

    Completed Block Scoping

---
+++ JS Scopes/app.js
@@ -0,0 +1,16 @@
+{
+    let x = 5;
+    let p = 2;
+    console.log(x);
+  }
+  console.log(p);
+
+//   if we use let in block of code it will not recognised globally
+
+{
+    var y = 9;
+    var z = 10;
+    console.log(y);
+}
+console.log(z);
+// z is defined as var is used
---
+++ JS Scopes/script.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+
+//     function log (num){
+//     console.log(num);
+//   }
+//   log(30);
+
+// function log(name,education,age) {
+//     console.log("my name is " + name + " and i have done " + education
+//     + " and my age is " + age);
+// }
+// log("alina","webdev",21);
+
+// var num1 = [1,2,3];
+// var num2 = [...num1,4,5,6];
+// console.log(num2);
+
+// var num = [6,2,5];
+// function addNum(a,b,c){
+//     console.log(a+b+c);
+// }
+// addNum(...num);
+
+//  var city1 = ["lahore","karachi","multan"];
+//      console.log(...city1);
+//  var city2 = [...city1,"faislabad","islamabad","hyderabad"];
+//   console.log(city2);
+
+// function logData(name,age){
+//     console.log(`my name is ${name} and my age is ${10+9}`);
+// }
+// logData("aneela" , 25);
+
+
+var carName = 'corolla';
+ 
+carName = 'civic';
+
+let carData = `this is a ${carName} car`;
+
+console.log(carData);
+
+
+}
+// so in console it will take the value in backticks
+
+
