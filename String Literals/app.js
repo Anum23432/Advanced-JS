576244f0e31640fc3f4a4ccf4ea291b19a71c2f1
Anum23432 <83118167+Anum23432@users.noreply.github.com>
2021-07-25 12:02:54 +0500

    Implementation of String Literals

---
+++ String Literals/app.js
@@ -0,0 +1,13 @@
+let name = "Civic";
+let make = "1994";
+let manufacturer = "Honda";
+
+let car = {
+    name,
+    make,
+    manufacturer
+}
+
+console.log(car.name, 'car name');
+console.log(car.make, 'car make');
+console.log(car.manufacturer, 'car manufacturer');
